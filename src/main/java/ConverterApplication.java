b292201640eed94e12d715a15020c7513ff20649
Adelaiten <karoltrzaska19@gmail.com>
2019-03-12 13:54:29 +0100

    switch case with enums

--- src/main/java/ConverterApplication.java
+++ src/main/java/ConverterApplication.java
@@ -1,9 +1,29 @@
+import enums.FileTypes;
+
 public class ConverterApplication {
+
     public static void main(String[] args) {
-        int LAST_ITEM = args.length-1;
-        if(!(args[LAST_ITEM].contains(".csv"))) {
+
+        int LAST_ITEM_INDEX = args.length-1;
+        int TYPE_INDEX = args.length-2;
+
+        if(args.length == 0 || !(args[LAST_ITEM_INDEX].contains(".csv"))) {
             System.out.println("No input file defined");
+
             System.exit(0);
         }
+
+        String type = args[TYPE_INDEX];
+        FileTypes fileType = FileTypes.getType(type);
+
+        switch(fileType) {
+            case JSON:
+                break;
+            case TABLE:
+                break;
+            case XML:
+                break;
+        }
+
     }
 }
