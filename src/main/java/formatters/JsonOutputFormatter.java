ec37f3402eebce4624b355af919d54d9f30dc1cc
Adelaiten <karoltrzaska19@gmail.com>
2019-03-12 17:22:05 +0100

    jsonoutputformatter printToConsole method

--- src/main/java/formatters/JsonOutputFormatter.java
+++ src/main/java/formatters/JsonOutputFormatter.java
@@ -3,7 +3,20 @@ package formatters;
 import java.util.List;
 
 public class JsonOutputFormatter implements OutputFormatter {
+
     public void printToConsole(List<String[]> data) {
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for(String[] array : data) {
+            sb.append("[");
+            for(String word : array) {
+                sb.append(",\"" + word + "\"");
+            }
+            sb.append("]");
+        }
+        sb.append("}");
+
+        System.out.println(sb.toString());
     }
 }
