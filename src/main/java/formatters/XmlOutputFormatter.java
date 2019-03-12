e11b4a64b9d4f1f1c1185af0da5c6c0865dbd895
Adelaiten <karoltrzaska19@gmail.com>
2019-03-12 17:22:24 +0100

    xmloutputformatter printToConsole method

--- src/main/java/formatters/XmlOutputFormatter.java
+++ src/main/java/formatters/XmlOutputFormatter.java
@@ -3,7 +3,19 @@ package formatters;
 import java.util.List;
 
 public class XmlOutputFormatter implements OutputFormatter {
-    public void printToConsole(List<String[]> data) {
 
+    public void printToConsole(List<String[]> data) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\"");
+        sb.append("<file-content>");
+        for(String[] array : data) {
+            sb.append("<line>");
+            for(String word : array) {
+                sb.append("<word>" + word + "</word>");
+            }
+            sb.append("</line>");
+        }
+        sb.append("</file-content>");
+        System.out.println(sb.toString());
     }
 }
