4cd39b6dc0eac8d22d34e84f3e4c58139f781237
Adelaiten <karoltrzaska19@gmail.com>
2019-03-12 13:54:44 +0100

    simple csv converter class

--- src/main/java/SimpleCsvConverter.java
+++ src/main/java/SimpleCsvConverter.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class SimpleCsvConverter {
+
+
+    void convert(String file, String outputFormat) {
+        System.out.println("I convert csv to output format");
+    }
+
+    void convert (String file) {
+        System.out.println("I convert csv to output format");
+    }
 }
