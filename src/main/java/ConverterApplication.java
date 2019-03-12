73ce4583abfaa03081236dedee4f4211f2f7c509
Adelaiten <karoltrzaska19@gmail.com>
2019-03-12 08:59:28 +0100

    exit when no file defined

--- src/main/java/ConverterApplication.java
+++ src/main/java/ConverterApplication.java
@@ -1,5 +1,9 @@
 public class ConverterApplication {
     public static void main(String[] args) {
-        
+        int LAST_ITEM = args.length-1;
+        if(!(args[LAST_ITEM].contains(".csv"))) {
+            System.out.println("No input file defined");
+            System.exit(0);
+        }
     }
 }
